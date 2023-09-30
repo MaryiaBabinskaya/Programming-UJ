a08acfaf188b6828508c576b5b671c52947b23a9
motia <94359114+MaryiaBabinskaya@users.noreply.github.com>
2023-09-30 14:05:10 +0300

    Add files via upload

---
+++ Zadanie Treningowe Podzielność/source (1).cpp
@@ -0,0 +1,21 @@
+//Maria Babinska
+#include <iostream> 
+using namespace std;
+int main(){
+	int N,M,liczba;
+	int n=0;
+	cin>>N>>M;
+	
+	for(int i=1;i<=M;i++){
+		cin>>liczba;
+		
+	if(N%liczba==0){
+		cout<<"TAK"<<endl;
+		n=1;
+		break;
+		}}
+	 if (n==0) {
+		cout<<"NIE"<<endl;
+		}
+	 
+	 }
